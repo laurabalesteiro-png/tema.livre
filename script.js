d3c8f9c4b6fd6f68e432e2388828042f608d4411
laurabalesteiro-png <laura.balesteiro@escola.pr.gov.br>
2026-08-27 22:29:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,28 @@ document.addEventListener("DOMContentLoaded", () => {
     // 2. Mensagem de Boas-Vindas no Console (Útil para testes no navegador)
     console.log("Website sobre aceitação e diversidade carregado com sucesso! 🌈");
 });
+<main>
+<img src="imagem-blog.png"
+alt="Logotipo conceitual de tecnologia e educacao: um livro aberto de onde emerge um cérebro digital brilhante, cercado por ícones de Wi-Fi, circuitos e lâmpada de ideia. Cores em tons de azul e branco.">
+
+<div>
+<h2>Meu primeiro post</h2>
+<p class="artigo-autor">Por: Marcelo Paludetto</p>
+<p>Boas-vindas ao meu novo blog! Aqui vou compartilhar dicas de programação
+e curiosidades da área de tecnologia.</p>
+<button>❤️<span>0</span></button>
+<button>👍<span>0</span></button>
+</div>
+
+<img src="imagem-blog.png"
+alt="Logotipo conceitual de tecnologia e educacao: um livro aberto de onde emerge um cérebro digital brilhante, cercado por ícones de Wi-Fi, circuitos e lâmpada de ideia. Cores em tons de azul e branco.">
+
+<div>
+<h2>Meu primeiro post</h2>
+<p class="artigo-autor">Por: Marcelo Paludetto</p>
+<p>Boas-vindas ao meu novo blog! Aqui vou compartilhar dicas de programação
+e curiosidades da área de tecnologia.</p>
+<button>❤️<span>0</span></button>
+<button>👍<span>0</span></button>
+</div>
+</main>
